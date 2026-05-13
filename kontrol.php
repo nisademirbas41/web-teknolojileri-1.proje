5d11559462febc9ec2f72cddd51bafd4f3baf9be
Nisa Demirbaş <nisademirbas41@gmail.com>
2026-05-13 23:56:14 +0300

    10. Gün: Giriş kontrol sistemi öğrenci bilgileriyle güncellendi

--- kontrol.php
+++ kontrol.php
@@ -1,14 +1,14 @@
 <?php
-// b2412100001 kısmını kendi okul numaranla güncellemeyi unutma
-$dogru_email = "b2412100001@sakarya.edu.tr";
-$dogru_sifre = "b2412100001";
+
+$dogru_email = "b251210003@sakarya.edu.tr";
+$dogru_sifre = "b251210003";
 
 if ($_POST) {
     $gelen_email = $_POST['email'];
     $gelen_sifre = $_POST['sifre'];
 
     if ($gelen_email == $dogru_email && $gelen_sifre == $dogru_sifre) {
-        header("Location: hosgeldiniz.php?no=b2412100001");
+        header("Location: hosgeldiniz.php?no=b251210003");
     } else {
         echo "<script>alert('Hatalı Giriş Bilgileri!'); window.location.href='login.php';</script>";
     }
